8ef7378835f5f8aa556312787d8f1788b6dee900
jeissonlazo <61564972+jeissonlazo@users.noreply.github.com>
2026-06-14 20:50:29 -0500

    feat: update validation for referral code length in JoinGroupDialog

--- src/components/groups/JoinGroupDialog.vue
+++ src/components/groups/JoinGroupDialog.vue
@@ -31,7 +31,7 @@ function handleJoin() {
 
   const referralCode = code.value.join('')
 
-  if (referralCode.length < 9) {
+  if (referralCode.length < 8) {
     message.warning('Ingresa el código completo')
     return
   }
